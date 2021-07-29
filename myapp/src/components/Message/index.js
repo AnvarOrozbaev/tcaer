f816fe7b51aca83c39c531db3e238db69a827aea
AnvarOrozbaev <80960576+AnvarOrozbaev@users.noreply.github.com>
2021-07-29 16:36:39 +0600

    Delete myapp directory

--- myapp/src/components/Message/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { usePrev } from "../../utils";
-
-export const Message = ({ text, author }) => (
-  <div>
-    <p className = "chat__text"><span className="auhor__text ">{author}</span>: {text}</p>
-  </div>
-);
